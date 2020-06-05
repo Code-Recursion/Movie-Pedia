6ef31cbff3237b0cf510a547ab8f48518bf06836
Ajay Singh <helloajaysingh1@gmail.com>
2020-06-05 20:11:01 +0530

    Updated UI

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#close-btn").click(function () {
+    $("#modal").css("display", "none");
+  });
+
+  $("#close-x").click(function () {
+    $("#modal").css("display", "none");
+  });
+});
